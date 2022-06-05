631f3cfffdf3d7c865f1feec58a31ec4e4a6e9b5
Angel Soto <100176750+Angeltheesoto@users.noreply.github.com>
2022-06-05 13:32:05 -0400

    Still having issues with getting locations to show on map

--- myproject/index.js
+++ myproject/index.js
@@ -1,4 +1,5 @@
 
+
 // leaflet code
 var map = L.map('map').setView([51.505, -0.09], 13);
 
@@ -21,75 +22,76 @@ const locationText = document.querySelector('.leaflet-popup-content');
 // locationText.textContent = `<span style="background-color: lime">Replacement HTML</span>`;
 
 
-
 // icon style
-const myIcon = L.icon({
-    iconUrl: 'images/icon2022.png',
-    iconSize: [50, 50],
-    iconAnchor: [22, 94],
-    popupAnchor: [-3, -76],
-});
-const marker = L.marker([50.505, 30.57], {icon: myIcon}).addTo(map);
-
+// const myIcon = L.icon({
+//     iconUrl: 'images/icon2022.png',
+//     iconSize: [50, 50],
+//     iconAnchor: [22, 94],
+//     popupAnchor: [-3, -76],
+// });
+// const marker = L.marker([50.505, 30.57], {icon: myIcon}).addTo(map);
 
 
 // create an event listener to pop up a specific location in the area for that specific list item for each of the list items.
 // have that evnet listener prompt some example text.
-// change the switch statement to an array
 
-
-// let buisness = document.getElementById('places').value;
-
-
-
+// shows the buisnesses lists when item is selected
 document.getElementById("places").addEventListener('change',
   (event) => {
-   event.preventDefault();
-
-    switch (event.target.value) {
+   let buisness = event.target.value;
+    switch (buisness) {
       case "coffee":
-       // options
-        let buisness = document.getElementById('places').value;
+        // console.log(buisness);
+        getFoursquare(buisness)
         break;
-      case "restauraunt":
-        console.log({
-          Message: "You have selected restauraunt"
-        });
+      case "restaurant":
+        getFoursquare(buisness)
         break;
       case "hotel":
-        console.log({
-          Message: "You have selected hotel"
-        });
+        getFoursquare(buisness)
         break;
       case "market":
-        console.log({
-          Message: "You have selected market"
-        });
+        getFoursquare(buisness)
+        break;
+      default:
+        console.log('default');
         break;
     }
-  } 
-);
+  });
 
 
-const options = {
- method: 'GET',
- headers: {
+// have foursquare find locations of each selected options show on the map 
+
+// get foursquare buisnesses
+async function getFoursquare(buisness) {
+  const options = {
+  method: 'GET',
+  headers: {
   Accept: 'application/json',
   Authorization: 'fsq3XcDK8dkjWj4yZXVUYXClvuc0vwuw9oaPk6X3rbyf0/I='
- }
+  }
 };
 
-fetch('https://api.foursquare.com/v3/places/search?&query=${buisness}', options)
-.then(response => response.json())
-.then(response => console.log(response))
-.catch(err => console.error(err));
+let response = await fetch(
+  `https://api.foursquare.com/v3/places/search?&query=${buisness}&limit=10`,
+options
+);
 
+let data = await response.text();
+let parsedData = JSON.parse(data);
+console.log(parsedData);
+return parsedData;
+}
 
-// have foursquare find locations of each selected options show on the map in the location
-
-
-
-
+// function to show the map where the locations are when clicked.
+function processBusinesses() {
+  const myIcon = L.icon({
+    iconUrl: 'images/icon2022.png',
+    iconSize: [50, 50],
+    iconAnchor: [22, 94],
+    popupAnchor: [-3, -76],
+  });
+}
 
 
 
